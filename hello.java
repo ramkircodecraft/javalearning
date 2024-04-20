3e1fd31ef1dad0a0053db0b7315072e2e14fd4d8
unknown <ramkirider@gmail.com>
2024-04-20 14:34:49 +0530

    added two files

---
+++ hello.java
@@ -0,0 +1,7 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello{
+    public static void main(String []args){
+        System.out.println("hello world");
+    }
+}
---
+++ javasample1.java
@@ -0,0 +1,10 @@
+import java.lang.System;
+import java.util.Scanner;
+class javasample1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int b = scan.nextInt();
+        System.out.println(b);
+    }
+    
+}
