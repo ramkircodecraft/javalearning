135feb62d75c3e286ea5f3b1ed1ac704a563974b
unknown <ramkirider@gmail.com>
2024-04-20 15:08:07 +0530

    created newfile

---
+++ javasample2.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+import java.util.Scanner;
+class javasample2{
+    public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     String name=scan.nextLine();
+     int age=scan.nextInt();
+     scan.nextLine();
+     String address=scan.nextLine();
+     System.out.println("my name is :"+name);
+     System.out.println("my age is :"+age);
+     System.out.println("my address is :"+address);  
+    }
+}
